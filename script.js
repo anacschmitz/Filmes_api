c406baa73d423942ecced07bb531571721b89141
Ana Carolina Schmitz da Silva <101817785+anacschmitz@users.noreply.github.com>
2023-10-05 21:34:01 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,34 @@
+let container = document.querySelector('.container');
+let cardMaior = document.querySelector('.cardMaior')
+let formulario = document.querySelector('.formulario');
+let tarefa = document.querySelector('.tarefa');
+
+formulario.addEventListener('submit', async function(event){
+  event.preventDefault();
+  let name = tarefa.value;
+  await getFilme(name); 
+})
+
+async function getFilme(name) {
+  let request = await fetch(`http://www.omdbapi.com/?s=${name}&apikey=e6c43dc6`);
+  let response = await request.json();
+  
+  let filmes = response.Search;
+
+  cardMaior.innerHTML = ''
+  filmes.forEach((filme) => {
+  let filmeName = filme['Title'];
+  let imagem = filme['Poster'];
+  
+
+  cardMaior.innerHTML += `
+    <div class="card">
+      <div class="image"> <img src="${imagem}" alt=""></div> 
+      <div>
+        <h2>| ${filmeName} | </h2>
+      </div>
+  `
+  });
+
+}
+
